d0089de55f582d4f78361c2134417238af08c3ec
Alex Goodman <alexthegoodman@gmail.com>
2023-04-16 20:14:09 -0400

    experimenting with getUserMedia

--- main/background.ts
+++ main/background.ts
@@ -1,32 +1,51 @@
-import { app } from 'electron';
-import serve from 'electron-serve';
-import { createWindow } from './helpers';
+import { app, ipcMain } from "electron";
+import serve from "electron-serve";
+import { createWindow } from "./helpers";
 
-const isProd: boolean = process.env.NODE_ENV === 'production';
+const { desktopCapturer } = require("electron");
+
+require("@electron/remote/main").initialize();
+
+const isProd: boolean = process.env.NODE_ENV === "production";
 
 if (isProd) {
-  serve({ directory: 'app' });
+  serve({ directory: "app" });
 } else {
-  app.setPath('userData', `${app.getPath('userData')} (development)`);
+  app.setPath("userData", `${app.getPath("userData")} (development)`);
 }
 
 (async () => {
   await app.whenReady();
 
-  const mainWindow = createWindow('main', {
+  const mainWindow = createWindow("main", {
     width: 1000,
     height: 600,
   });
 
   if (isProd) {
-    await mainWindow.loadURL('app://./home.html');
+    await mainWindow.loadURL("app://./home.html");
   } else {
     const port = process.argv[2];
     await mainWindow.loadURL(`http://localhost:${port}/home`);
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
   }
 })();
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   app.quit();
+});
+
+// ipcMain.on("ping-pong", (event, arg) => {
+//   event.sender.send("ping-pong", `[ipcMain] "${arg}" received asynchronously.`);
+// });
+
+ipcMain.on("test", (event, arg) => {
+  let mousePositions = [];
+  let startTime = Date.now();
+
+  desktopCapturer.getSources({ types: ["screen"] }).then((sources) => {
+    event.returnValue = sources;
+
+    // if (error) throw error;
+  });
 });
--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,8 +1,110 @@
-import React from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import electron from "electron";
+
+const ipcRenderer = electron.ipcRenderer;
+
+// const remote = require("@electron/remote");
+// import remote from "@electron/remote";
 
 function Home() {
+  const [message, setMessage] = React.useState("no ipc message");
+
+  const onClickWithIpcSync = () => {
+    const sources = ipcRenderer.sendSync("test");
+
+    console.info("source", sources[0].id);
+
+    let mousePositions = [];
+    let startTime = Date.now();
+
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: {
+          mandatory: {
+            chromeMediaSource: "desktop",
+            chromeMediaSourceId: sources[0].id,
+            minWidth: 1280,
+            maxWidth: 1280,
+            minHeight: 720,
+            maxHeight: 720,
+          },
+        },
+      })
+      .then((stream) => {
+        console.info("stream", stream);
+        const video = document.getElementById("testvideo") as HTMLVideoElement;
+        video.srcObject = stream;
+        video.width = 1280;
+        video.height = 720;
+        // video.play();
+        video.onloadedmetadata = (e) => video.play();
+        // console.info("video", video);
+        // const canvas = document.createElement("canvas");
+        // const ctx = canvas.getContext("2d");
+        // canvas.width = 1280;
+        // canvas.height = 720;
+        // const captureInterval = setInterval(() => {
+        //   ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        //   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        //   const mousePosition = remote.screen.getCursorScreenPoint();
+        //   mousePositions.push({
+        //     x: mousePosition.x,
+        //     y: mousePosition.y,
+        //     timestamp: Date.now() - startTime,
+        //   });
+        // }, 100);
+        // const stopRecording = () => {
+        //   clearInterval(captureInterval);
+        //   stream.getTracks()[0].stop();
+        //   const blob = new Blob(chunks, { type: "video/webm" });
+        //   const url = URL.createObjectURL(blob);
+        //   const videoElement = document.createElement("video");
+        //   videoElement.src = url;
+        //   document.body.appendChild(videoElement);
+        //   const mousePositionsBlob = new Blob(
+        //     [JSON.stringify(mousePositions)],
+        //     { type: "application/json" }
+        //   );
+        //   const mousePositionsUrl = URL.createObjectURL(mousePositionsBlob);
+        //   const a = document.createElement("a");
+        //   a.href = mousePositionsUrl;
+        //   a.download = "mouse_positions.json";
+        //   a.click();
+        // };
+        // const chunks = [];
+        // const mediaRecorder = new MediaRecorder(stream, {
+        //   mimeType: "video/webm; codecs=vp9",
+        // });
+        // mediaRecorder.ondataavailable = (e) => chunks.push(e.data);
+        // mediaRecorder.onstop = stopRecording;
+        // mediaRecorder.start();
+        // setTimeout(() => mediaRecorder.stop(), 5000);
+      })
+      .catch((error) => console.log(error));
+
+    setMessage(JSON.stringify(sources));
+  };
+
+  // If we use ipcRenderer in this scope, we must check the instance exists
+  if (ipcRenderer) {
+    // In this scope, the webpack process is the client
+  }
+
+  React.useEffect(() => {
+    ipcRenderer.on("ping-pong", (event, data) => {
+      setMessage(data);
+    });
+
+    // onClickWithIpc();
+
+    return () => {
+      ipcRenderer.removeAllListeners("ping-pong");
+    };
+  }, []);
+
   return (
     <React.Fragment>
       <Head>
@@ -10,7 +112,9 @@ function Home() {
       </Head>
       <div>
         <p>
-          ⚡ Electron + Next.js ⚡ -
+          ⚡ Electron + Next.js ⚡ - {message}
+          <video id="testvideo"></video>
+          <button onClick={onClickWithIpcSync}>Test</button>
           <Link href="/next">
             <a>Go to next page</a>
           </Link>
@@ -19,6 +123,6 @@ function Home() {
       </div>
     </React.Fragment>
   );
-};
+}
 
 export default Home;
